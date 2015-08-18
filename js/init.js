6f6f383e534f7fbf6270180aa91cb9e7bdcbdb44
satanas <wil.alejandro@gmail.com>
2015-08-18 14:24:16 -0300

    Added more reimplementations and refactored functions

--- js/init.js
+++ js/init.js
@@ -5,3 +5,31 @@ window.raf = window.requestAnimationFrame ||
 
 window.caf = window.cancelAnimationFrame ||
   window.mozCancelAnimationFrame;
+
+var $ = {},
+    abs = Math.abs,
+    cos = Math.cos,
+    sin = Math.sin,
+    ceil = Math.ceil,
+    floor = Math.floor,
+    max = Math.max,
+    pow = Math.pow,
+    sqrt = Math.sqrt,
+    round = Math.round,
+    rnd = Math.random,
+    now = Date.now;
+
+// Gets a DOM element by id
+$.byId = function(i) {
+  return document.getElementById(i);
+};
+
+// Shows a DOM object putting its opacity in one
+$.show = function(i) {
+  $.u.byId(i).style.opacity = 1.0;
+};
+
+// Hides a DOM object putting its opacity in zero
+$.hide = function(i) {
+  $.u.byId(i).style.opacity = 0.0;
+};

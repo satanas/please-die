a8ac7ed7141c345f7d5efb148699bcc3886aa337
satanas <wil.alejandro@gmail.com>
2015-08-19 14:24:21 -0300

    Included input in game.js along with menu scene. Added method to test input in menu

--- js/game.js
+++ js/game.js
@@ -5,9 +5,28 @@ window.addEventListener('load', function() {
 
 $.start = function() {
   $.init('c', 640, 480);
+  $.i = new Input();
+  $.i.b([13, 65, 37, 38, 39, 40, 49, 50, 51, 52]);
+  $.menu = new Menu();
+  $.menu.loop();
 };
 
-$.menu = function() {
-  this.loop = function() {
+var Menu = function() {
+  var _ = this;
+  _.loop = function() {
+    $.x.clr();
+
+    if ($.i.r(13)) {
+      console.log('Enter');
+    } else if ($.i.p(37)) {
+      console.log('37');
+    } else if ($.i.p(39)) {
+      console.log('39');
+    } else if ($.i.p(38)) {
+      console.log('38');
+    } else if ($.i.p(40)) {
+      console.log('40');
+    }
+    raf(_.loop.bind(_));
   };
 };

235ea61c827f5f6ac247c36daf53135de40e2d14
satanas <wil.alejandro@gmail.com>
2015-08-18 23:24:23 -0300

    Fixed some methods in init.js and added method with canvas initialization

--- js/init.js
+++ js/init.js
@@ -19,6 +19,27 @@ var $ = {},
     rnd = Math.random,
     now = Date.now;
 
+// Initialize all variables
+// c: canvas id
+// w: width
+// h: height
+$.init = function(c, w, h) {
+  // Get canvas
+  $.c = $.byId(c);
+  // Get canvas context
+  $.x = $.c.getContext('2d');
+  // Rename common used methods
+  $.x.s = $.x.save;
+  $.x.r = $.x.restore;
+  $.x.fr = $.x.fillRect;
+  $.x.ft = $.x.fillText;
+  $.x.d = $.x.drawImage;
+  $.x.sc = $.x.scale;
+
+  $.vw = $.c.width  = w;
+  $.vh = $.c.height = h;
+};
+
 // Gets a DOM element by id
 $.byId = function(i) {
   return document.getElementById(i);
@@ -26,10 +47,10 @@ $.byId = function(i) {
 
 // Shows a DOM object putting its opacity in one
 $.show = function(i) {
-  $.u.byId(i).style.opacity = 1.0;
+  $.byId(i).style.opacity = 1;
 };
 
 // Hides a DOM object putting its opacity in zero
 $.hide = function(i) {
-  $.u.byId(i).style.opacity = 0.0;
+  $.byId(i).style.opacity = 0;
 };

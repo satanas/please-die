012c0e73df178d509fbf9432275587c2b601ca54
satanas <wil.alejandro@gmail.com>
2015-08-20 14:25:53 -0300

    Added menu and game scenes

--- js/game.js
+++ js/game.js
@@ -7,25 +7,39 @@ $.start = function() {
   $.init('c', 640, 480);
   $.i = new Input();
   $.i.b([13, 65, 37, 38, 39, 40, 49, 50, 51, 52]);
-  $.menu = new Menu();
+  $.menu = new MenuScene();
+  $.game = new GameScene();
+
   $.menu.loop();
 };
 
-var Menu = function() {
+var MenuScene = function() {
   var _ = this;
   _.loop = function() {
     $.x.clr();
 
-    if ($.i.r(13)) {
+    $.x.s();
+    //$.x.font = "35px serif";
+    $.x.fs = "rgb(255,255,255)";
+    $.x.ft('Die Fast', 200, 200);
+    $.x.r();
+
+    if ($.i.p(13)) {
       console.log('Enter');
-    } else if ($.i.p(37)) {
-      console.log('37');
-    } else if ($.i.p(39)) {
-      console.log('39');
-    } else if ($.i.p(38)) {
-      console.log('38');
-    } else if ($.i.p(40)) {
-      console.log('40');
+      $.game.loop();
+    }
+    raf(_.loop.bind(_));
+  };
+};
+
+var GameScene = function() {
+  var _ = this;
+  console.log('laaskldjalksdj');
+  _.loop = function() {
+    $.x.clr();
+
+    if ($.i.p(13)) {
+      console.log('Enter');
     }
     raf(_.loop.bind(_));
   };

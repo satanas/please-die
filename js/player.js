3626719e07c526320d868364f37c97faf15ff897
satanas <wil.alejandro@gmail.com>
2015-08-21 10:42:27 -0300

    Added accelerated movement. Refactored fillstyle

--- js/player.js
+++ js/player.js
@@ -4,10 +4,29 @@ var Player = function(x, y) {
   _.y = y;
   _.w = 16;
   _.h = 32;
+  _.dx = 0;
+  _.s = 0.23; // Speed
+  _.ms = 6; // Max speed
+
+  _.u = function() {
+    if ($.i.p(37)) {
+      _.dx -= _.s;
+    } else if ($.i.p(39)) {
+      _.dx += _.s;
+    }
+
+    _.dx = iir(_.dx, -_.ms, _.ms);
+    if (!$.i.p(37) && !$.i.p(39)) {
+      _.dx = 0;
+    }
+
+    _.x += _.dx;
+    //_.y += _.dy;
+  };
 
   _.r = function() {
     $.x.s();
-    $.x.fillStyle = "white";
+    $.x.fs("white");
     $.x.fr(_.x, _.y, _.w, _.h);
     $.x.r();
   };

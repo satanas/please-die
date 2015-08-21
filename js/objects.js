0b343c8824aa66a68475815414b3c516764991b5
satanas <wil.alejandro@gmail.com>
2015-08-21 18:41:04 -0300

    Added bounds to blocks

--- js/objects.js
+++ js/objects.js
@@ -2,8 +2,15 @@ var Block = function(x, y) {
   var _ = this;
   _.x = x;
   _.y = y;
-  _.w = 32;
-  _.h = 32;
+  _.w = 32; // Width
+  _.h = 32; // Height
+  // Bounds
+  _.b = {
+      b: _.y + _.h,
+      t: _.y,
+      l: _.x,
+      r: _.x + _.w
+    };
 
   _.r = function() {
     $.x.s();

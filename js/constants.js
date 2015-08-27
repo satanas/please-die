45a11fdbc2ae7f7f5111898001671e230500b4b9
satanas <wil.alejandro@gmail.com>
2015-08-26 23:59:13 -0300

    Adjusting effect time for electricity

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 // Constants to define the value (v), time of effect (t) and damage (d) of traps
 $.BL = {v: 1, t: 1200, d: 2.5}; // Bleeding
 $.BU = {v: 2, t: 2000, d: 2.5}; // Burning
-$.EL = {v: 4, t: 1000, d: 2.5}; // Electrocuting
+$.EL = {v: 4, t: 1800, d: 2.5}; // Electrocuting
 $.PO = {v: 8, t: 3000, d: 2.5}; // Poisoning/Radiation

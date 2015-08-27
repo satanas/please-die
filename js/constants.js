107903d4925cc21abe47b7fc8006912d150f63b1
satanas <wil.alejandro@gmail.com>
2015-08-26 22:44:35 -0300

    Added more traps attribute (time of effect, damage)

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,5 @@
-$.B = 1; // Bleeding
-$.U = 2; // Burning
-$.E = 3; // Electrocuting
+// Constants to define the value (v), time of effect (t) and damage (d) of traps
+$.BL = {v: 1, t: 1200, d: 2.5}; // Bleeding
+$.BU = {v: 2, t: 2000, d: 2.5}; // Burning
+$.EL = {v: 4, t: 1000, d: 2.5}; // Electrocuting
+$.PO = {v: 8, t: 3000, d: 2.5}; // Poisoning/Radiation

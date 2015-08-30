b0576dc7341f1fae3073e98ec1cae74ccb33e818
satanas <wil.alejandro@gmail.com>
2015-08-30 12:05:32 -0300

    Adjusted some damage numbers

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
-// Constants to define the value (v), time of effect (t) and damage (d) of traps
+// Constants to define the value (v), time of effect (t), unique damage (d) and damage per second (ds) of traps
 $.BL = {v: 1, t: 1200, d: 2.5}; // Bleeding
-$.BU = {v: 2, t: 2000, d: 2.5}; // Burning
-$.EL = {v: 4, t: 1800, d: 2.5}; // Electrocuting
+$.BU = {v: 2, t: 2000, d: 15, ds: 2.6}; // Burning
+$.EL = {v: 4, t: 1800, d: 50}; // Electrocuting
 $.PO = {v: 8, t: 3000, d: 2.5}; // Poisoning/Radiation

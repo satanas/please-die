e85d2e1d71fd18a48776499fd19bac25b0150983
satanas <wil.alejandro@gmail.com>
2015-08-30 21:02:16 -0300

    Blocked movement after dead

--- js/player.js
+++ js/player.js
@@ -18,90 +18,93 @@ var Player = function(x, y) {
   _.s = 0.53; // Speed
   _.mxs = 5; // Max x speed
   _.mys = 15; // Max y speed
-  _.hl = 1000; // Health
+  _.hl = 100; // Health
   _.e = new Emitter(); // Particles emitter
 
   _.u = function() {
     //console.log('hu', _.hu, 'blc', _.blc, 'elc', _.elc, 'ic', _.ic);
-    // If invincible, decrease counter
-    //_.ic = dcz(_.ic, $.e);
-    if (_.ic !== 0) {
-      _.ic -= $.e;
-      if (_.ic <= 0) _.ic = 0;
-    }
 
-    // If bleeding, recover
-    if (_.blc !== 0) {
-      _.blc -= $.e;
-      if (_.blc <= 0) {
-        _.blc = 0;
-        _.hu -= $.BL.v;
+    if (_.a) {
+      //_.ic = dcz(_.ic, $.e);
+      // If invincible, decrease counter
+      if (_.ic !== 0) {
+        _.ic -= $.e;
+        if (_.ic <= 0) _.ic = 0;
       }
-    }
 
-    // If burning, recover
-    if (_.buc !== 0) {
-      $.s.p('bu');
-      _.e.e(_.x, _.y, 6, 2);
-      _.buc -= $.e;
-      _.hl -= ($.e * $.BU.ds) / 1000;
-      if (_.buc <= 0) {
-        _.buc = 0;
-        _.hu -= $.BU.v;
+      // If bleeding, recover
+      if (_.blc !== 0) {
+        _.blc -= $.e;
+        if (_.blc <= 0) {
+          _.blc = 0;
+          _.hu -= $.BL.v;
+        }
       }
-    }
 
-    // If electrocuting, recover
-    if (_.elc !== 0) {
-      $.s.p('el');
-      _.e.e(_.x, _.y + (_.h / 2), 10, 3);
-      _.elc -= $.e;
-      _.hl -= $.EL.d;
-      if (_.elc <= 0) {
-        _.elc = 0;
-        _.hu -= $.EL.v;
+      // If burning, recover
+      if (_.buc !== 0) {
+        $.s.p('bu');
+        _.e.e(_.x, _.y, 6, 2);
+        _.buc -= $.e;
+        _.hl -= ($.e * $.BU.ds) / 1000;
+        if (_.buc <= 0) {
+          _.buc = 0;
+          _.hu -= $.BU.v;
+        }
       }
-    }
 
-    if (_.hu > 0) {
-      _.bkc += $.e;
-      if (_.bkc > 150) {
-        _.bkc = 0;
-        _.bk = !_.bk;
+      // If electrocuting, recover
+      if (_.elc !== 0) {
+        $.s.p('el');
+        _.e.e(_.x, _.y + (_.h / 2), 10, 3);
+        _.elc -= $.e;
+        _.hl -= $.EL.d;
+        if (_.elc <= 0) {
+          _.elc = 0;
+          _.hu -= $.EL.v;
+        }
       }
-    } else {
-      _.bk = 0;
+
+      if (_.hu > 0) {
+        _.bkc += $.e;
+        if (_.bkc > 150) {
+          _.bkc = 0;
+          _.bk = !_.bk;
+        }
+      } else {
+        _.bk = 0;
+      }
+
+      var mxs = _.mxs; // Max x speed that will be affected by traps
+      var mys = _.mys; // May y speed that will be affected by traps
+      if (_.hu & $.BL.v) mxs = _.mxs / 3;
+      if (_.hu & $.BU.v) mxs = _.mxs * 1.5;
+      if (_.hu & $.EL.v) { mxs = 0; mys = 0}
+
+      // Side movement
+      if ($.i.p(37)) {
+        _.dx -= _.s;
+      } else if ($.i.p(39)) {
+        _.dx += _.s;
+      }
+
+      _.dx = iir(_.dx, -mxs, mxs);
+      if (!$.i.p(37) && !$.i.p(39)) {
+        _.dx = 0;
+      }
+
+      // Jump
+      if (_.dy === 0 && $.i.p(38) && !(_.hu & $.EL.v)) {
+        $.s.p('j');
+        _.dy = -8;
+      }
+
+      _.dy += 19.8 * ($.e / 1000);
+      _.dy = iir(_.dy, -mys, mys);
+
+      _.x += _.dx;
+      _.y += _.dy;
     }
-
-    var mxs = _.mxs; // Max x speed that will be affected by traps
-    var mys = _.mys; // May y speed that will be affected by traps
-    if (_.hu & $.BL.v) mxs = _.mxs / 3;
-    if (_.hu & $.BU.v) mxs = _.mxs * 1.5;
-    if (_.hu & $.EL.v) { mxs = 0; mys = 0}
-
-    // Side movement
-    if ($.i.p(37)) {
-      _.dx -= _.s;
-    } else if ($.i.p(39)) {
-      _.dx += _.s;
-    }
-
-    _.dx = iir(_.dx, -mxs, mxs);
-    if (!$.i.p(37) && !$.i.p(39)) {
-      _.dx = 0;
-    }
-
-    // Jump
-    if (_.dy === 0 && $.i.p(38) && !(_.hu & $.EL.v)) {
-      $.s.p('j');
-      _.dy = -8;
-    }
-
-    _.dy += 19.8 * ($.e / 1000);
-    _.dy = iir(_.dy, -mys, mys);
-
-    _.x += _.dx;
-    _.y += _.dy;
     // Recalculate bounds after movement
     _.rb();
 
@@ -124,7 +127,7 @@ var Player = function(x, y) {
 
     // Check collisions with traps
     $.g.t.forEach(function(w) {
-      if ($.o.rect(_, w)) {
+      if ($.o.rect(_, w) && _.a) {
         if (_.ic === 0) {
           _.ic = _.it;
           if (w.t === $.BL.v) {
@@ -146,6 +149,8 @@ var Player = function(x, y) {
         }
       }
     });
+
+    if (_.hl <= 0) _.a = 0;
 
     // Update emitter
     _.e.u();

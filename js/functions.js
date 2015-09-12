5b5727456e5277370a7236222b0fb550fb337ef4
satanas <wil.alejandro@gmail.com>
2015-09-11 22:21:55 -0300

    Added function to decrement and reset

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 // Decrement until zero.
 // v: value
 // d: decrement
+// cb: callback to execute when v reaches zero
 // Returns the value decremented
 var duz = function(v, d, cb) {
   if (v !== 0) {
@@ -10,6 +11,20 @@ var duz = function(v, d, cb) {
       if (cb !== undefined) cb();
     }
   } else {
+    if (cb !== undefined) cb();
+  }
+  return v;
+};
+
+// Decrement until zero and reset
+// v: value
+// d: Decrement
+// r: value to reset
+// cb: callback to execute when v reaches zero
+var drst = function(v, d, r, cb) {
+  v -= d;
+  if (v <= 0) {
+    v = r;
     if (cb !== undefined) cb();
   }
   return v;

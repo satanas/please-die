07b66308a65c56faf4c003d5f7a41f72091f1efb
satanas <wil.alejandro@gmail.com>
2015-09-12 11:54:04 -0300

    Added function to draw flowers when in rainbow

--- js/functions.js
+++ js/functions.js
@@ -29,3 +29,25 @@ var drst = function(v, d, r, cb) {
   }
   return v;
 };
+
+// Draw flower (used by traps and some triggers)
+var drwf = function(r, _) {
+  $.x.fs("red");
+  $.x.bp();
+  $.x.arc(r.x - 10, r.y - 10, _.w / 3, 0, 2 * PI);
+  $.x.f();
+  $.x.bp();
+  $.x.arc(r.x - 10, r.y + 10, _.w / 3, 0, 2 * PI);
+  $.x.f();
+  $.x.bp();
+  $.x.arc(r.x + 10, r.y - 10, _.w / 3, 0, 2 * PI);
+  $.x.f();
+  $.x.bp();
+  $.x.arc(r.x + 10, r.y + 10, _.w / 3, 0, 2 * PI);
+  $.x.f();
+
+  $.x.bp();
+  $.x.fs("yellow");
+  $.x.arc(r.x, r.y, _.w / 3, 0, 2 * PI);
+  $.x.f();
+};
